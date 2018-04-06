c45fb0f424ade8231bbe5b0f17571ff1745c237e
Tobi2K <tobika2000.tk@gmail.com>
2018-04-06 22:43:25 +0200

    icons

--- modulemanager/dashboard/include.php
+++ modulemanager/dashboard/include.php
@@ -1,65 +1,63 @@
-<div class="row">
-    <div class="col-lg-3 col-md-6 col-sm-6">
-        <div class="card card-stats">
-            <a href="#">
-                <div class="card-header" data-background-color="grey">
-                    <i class="material-icons">settings</i>
-                </div>
-            </a>
-            <div class="card-content">
-                <p class="category">Installed Modules</p>
-                <h3 class="title"><?= count($db->getModules()) - 1 ?></h3>
-                <?php
-                $includeables = 0;
-                $interfacefiles = 0;
-                $navs = 0;
-                foreach ($db->getModules() as $module) {
-                    if ($module->getName() != "Module Manager") {
-                        $includeables += count($module->getIncludeables());
-                        $interfacefiles += count($module->getDashboards());
-                        $navs += count($module->getNavs());
-                    }
-                }
-                ?>
+<div class="col-lg-3 col-md-6 col-sm-6">
+    <div class="card card-stats">
+        <a href="#">
+            <div class="card-header" data-background-color="grey">
+                <i class="mdi mdi-settings mdi-24px"></i>
             </div>
-            <div class="card-footer">
-                <div class="stats">
-                    <p><i class="material-icons">access_time</i> Includeables: <?= $includeables ?>
-                        <i class="material-icons">access_time</i> Dashboard Files: <?= $interfacefiles ?>
-                        <i class="material-icons">access_time</i> Navbar Links: <?= $navs ?></p>
-                </div>
+        </a>
+        <div class="card-content">
+            <p class="category">Installed Modules</p>
+            <h3 class="title"><?= count($db->getModules()) - 1 ?></h3>
+            <?php
+            $includeables = 0;
+            $interfacefiles = 0;
+            $navs = 0;
+            foreach ($db->getModules() as $module) {
+                if ($module->getName() != "Module Manager") {
+                    $includeables += count($module->getIncludeables());
+                    $interfacefiles += count($module->getDashboards());
+                    $navs += count($module->getNavs());
+                }
+            }
+            ?>
+        </div>
+        <div class="card-footer">
+            <div class="stats">
+                <p><i class="mdi mdi-clock mdi-24px"></i> Includeables: <?= $includeables ?> <br>
+                    <i class="mdi mdi-clock mdi-24px"></i> Dashboard Files: <?= $interfacefiles ?> <br>
+                    <i class="mdi mdi-clock mdi-24px"></i> Navbar Links: <?= $navs ?></p> <br>
             </div>
         </div>
     </div>
-    <div class="col-lg-3 col-md-6 col-sm-6">
-        <div class="card card-stats">
-            <a href="#">
-                <div class="card-header" data-background-color="grey">
-                    <i class="material-icons">settings</i>
-                </div>
-            </a>
-            <div class="card-content">
-                <p class="category">Installed Modules</p>
-                <h3 class="title"><?= count($db->getModules()) - 1 ?></h3>
-                <?php
-                $includeables = 0;
-                $interfacefiles = 0;
-                $navs = 0;
-                foreach ($db->getModules() as $module) {
-                    if ($module->getName() != "Module Manager") {
-                        $includeables += count($module->getIncludeables());
-                        $interfacefiles += count($module->getDashboards());
-                        $navs += count($module->getNavs());
-                    }
-                }
-                ?>
+</div>
+<div class="col-lg-3 col-md-6 col-sm-6">
+    <div class="card card-stats">
+        <a href="#">
+            <div class="card-header" data-background-color="grey">
+                <i class="mdi mdi-settings mdi-24px"></i>
             </div>
-            <div class="card-footer">
-                <div class="stats">
-                    <p><i class="material-icons">access_time</i> Includeables: <?= $includeables ?>
-                        <i class="material-icons">access_time</i> Dashboard Files: <?= $interfacefiles ?>
-                        <i class="material-icons">access_time</i> Navbar Links: <?= $navs ?></p>
-                </div>
+        </a>
+        <div class="card-content">
+            <p class="category">Installed Modules</p>
+            <h3 class="title"><?= count($db->getModules()) - 1 ?></h3>
+            <?php
+            $includeables = 0;
+            $interfacefiles = 0;
+            $navs = 0;
+            foreach ($db->getModules() as $module) {
+                if ($module->getName() != "Module Manager") {
+                    $includeables += count($module->getIncludeables());
+                    $interfacefiles += count($module->getDashboards());
+                    $navs += count($module->getNavs());
+                }
+            }
+            ?>
+        </div>
+        <div class="card-footer">
+            <div class="stats">
+                <p><i class="mdi mdi-clock mdi-24px"></i> Includeables: <?= $includeables ?> <br>
+                    <i class="mdi mdi-clock mdi-24px"></i> Dashboard Files: <?= $interfacefiles ?> <br>
+                    <i class="mdi mdi-clock mdi-24px"></i> Navbar Links: <?= $navs ?></p> <br>
             </div>
         </div>
     </div>
